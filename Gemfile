734db118b041729737c72c03c2a96eb32374b278
Brian Hurst <brian.hurst@gsa.gov>
2019-04-25 11:13:03 -0400

    Add redirect to demo site

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source "https://rubygems.org"
 
 gem "jekyll", "~> 3.8.3"
-gem "jekyll-sitemap"
-gem "html-proofer"
--- _js/index.js
+++
@@ -1,5 +0,0 @@
-var SmoothScroll = require("smooth-scroll");
-require("./contacts.js");
-require("./glossary.js");
-
-new SmoothScroll('a[href*="#"]');
