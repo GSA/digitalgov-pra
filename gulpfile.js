2d3ee0f9001e1810c01f365844f6036af7750b15
Nick Lyons <nick.lyons@gsa.gov>
2022-08-03 15:20:41 -0400

    removed img,fonts for new commit

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,10 @@ const uswds = require("@uswds/compile");
 uswds.settings.version = 3;
 uswds.paths.dist.theme = './_scss';
 uswds.paths.dist.css = "./_site/assets/uswds/css";
+uswds.paths.dist.img = "./assets/uswds/img"
+
 
 exports.init = uswds.init;
+exports.copyImages = uswds.copyImages;
 exports.compile = uswds.compile;
 exports.watch = uswds.watch;
