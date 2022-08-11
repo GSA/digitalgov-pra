bf735c4ebf8b29f7244f8a075efc14d9e8378de7
Nick Lyons <nick.lyons@gsa.gov>
2022-08-11 14:40:47 -0400

    fixed sass compilation

--- gulpfile.js
+++ gulpfile.js
@@ -13,13 +13,16 @@ USWDS SASS GULPFILE
 
 const uswds = require("@uswds/compile");
 uswds.settings.version = 3;
-uswds.paths.dist.css = "./_site/assets/uswds/css";
-uswds.paths.dist.img = "./assets/uswds/img"
 
+// uswds.paths.src.sass = './_scss';
+// uswds.paths.src.theme = '_scss';
+uswds.paths.src.projectSass = "./_scss";
 
-exports.init = uswds.init;
-exports.copyFonts = uswds.copyFonts;
-exports.copyImages = uswds.copyImages;
+uswds.paths.dist.css = "./assets/uswds/css";
+uswds.paths.dist.img = "./assets/uswds/img";
+uswds.paths.dist.theme = "./_scss"; // looks to compile theme from here
+
 exports.updateUswds = uswds.updateUswds;
+exports.compileSass = uswds.compileSass;
 exports.compile = uswds.compile;
 exports.watch = uswds.watch;
