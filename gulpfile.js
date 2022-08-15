a9489cb79f2fd4b2464b6a1f11e1d41a94ce8ae7
Nick Lyons <nick.lyons@gsa.gov>
2022-08-15 15:37:27 -0400

    fixed load path with styles.css

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,11 @@ USWDS SASS GULPFILE
 const uswds = require("@uswds/compile");
 uswds.settings.version = 3;
 
-// uswds.paths.src.sass = './_scss';
-// uswds.paths.src.theme = '_scss';
 uswds.paths.src.projectSass = "./_scss";
 
 uswds.paths.dist.css = "./assets/uswds/css";
 uswds.paths.dist.img = "./assets/uswds/img";
-uswds.paths.dist.theme = "./_scss"; // looks to compile theme from here
+uswds.paths.dist.theme = "./_scss";
 
 exports.updateUswds = uswds.updateUswds;
 exports.compileSass = uswds.compileSass;
